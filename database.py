7888bc425c23716a1de9ce4c4cd51530ec7a0cc9
RoshanPriyan <priyanhari2303@gmail.com>
2025-09-27 18:07:16 +0530

    use dotenv library

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME")
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # Database URL format: mysql+mysqlconnector://username:password@host:port/db_name
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Roshan%4020@localhost:3306/user_app"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Roshan%4020@localhost:3306/user_app"
 
 # Create SQLAlchemy engine
 engine = create_engine(
